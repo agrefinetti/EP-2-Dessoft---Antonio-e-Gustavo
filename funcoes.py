2ec8143096a8f25e5d214dcb108a63a4437ab42f
agrefinetti <agrefinetti@gmail.com>
2026-04-24 22:39:15 -0300

    exercício 1 completo

--- funcoes.py
+++ funcoes.py
@@ -3,9 +3,10 @@ import random
 def rolar_dados(n):
 
     resposta =[]
+    
     i = 0
 
     while i < n:
         resposta.append(random.randint(1,6))
         i +=1
-    return resposta      
+    return resposta    
