8b695396544a654cfc4a22fe6fabe63d59537e04
GustavoSchmidt123 <gustavolsf@al.insper.edu.br>
2026-04-24 22:00:23 -0300

    Add files via upload

---
+++ funcoes.py
@@ -0,0 +1,12 @@
+import random
+
+def rolar_dados(n):
+
+    resposta =[]
+    i = 0
+
+    while i < n:
+        resposta.append(random.randint(1,6))
+        i +=1
+    return resposta
+                        
