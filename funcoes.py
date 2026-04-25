ce3e5778087a09d3b8cb8d4b1a70e6eaf9bd58cf
GustavoSchmidt123 <gustavolsf@al.insper.edu.br>
2026-04-24 22:50:09 -0300

    Exercicio 2 completo

--- funcoes.py
+++ funcoes.py
@@ -10,3 +10,10 @@ def rolar_dados(n):
         resposta.append(random.randint(1,6))
         i +=1
     return resposta    
+
+def guardar_dado(dados_rolados, dados_guardados, indice):
+
+    dado = dados_rolados.pop(indice)
+    
+    dados_guardados.append(dado)
+    return [dados_rolados, dados_guardados]        
