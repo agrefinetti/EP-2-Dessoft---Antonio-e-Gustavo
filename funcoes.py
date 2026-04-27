da9647dbd8c98e7dd5f638ff91b7d8fdc3ac3409
GustavoSchmidt123 <gustavolsf@al.insper.edu.br>
2026-04-27 19:00:41 -0300

    exercicio 8 completo

--- funcoes.py
+++ funcoes.py
@@ -56,9 +56,27 @@ def calcula_pontos_sequencia_baixa(lista_inteiros):
     if (3 in lista_inteiros and 4 in lista_inteiros and 5 in lista_inteiros and 6 in lista_inteiros):
         return 15   
     return 0   
+
 def calcula_pontos_sequencia_alta(lista):
     if (1 in lista and 2 in lista and 3 in lista and 4 in lista and 5 in lista):
         return 30
     if (2 in lista and 3 in lista and 4 in lista and 5 in lista and 6 in lista):
         return 30
     return 0
+def calcula_pontos_full_house(lista):
+    contagens = []
+
+    for numero in lista:
+        qtd = lista.count(numero)
+        if qtd not in contagens:
+            contagens.append(qtd)
+
+    if 3 in contagens and 2 in contagens:
+        total = 0
+        i = 0
+        while i < len(lista):
+            total += lista[i]
+            i += 1
+        return total
+    else:
+        return 0
